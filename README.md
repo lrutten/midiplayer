421c2e23b545047361c0c8e5bda20805a117f2af
lrutten <lrutten@users.noreply.github.com>
2019-08-22 10:11:48 +0200

    SPI pins defined

--- README.md
+++ README.md
@@ -25,3 +25,25 @@ Nog te documenteren.
 
     Een `mbed-os` voorbeeld dat de sd card leest.
 
+
+## Aansluiting sdcard lezer
+
+In dit bestand
+
+~~~~
+mbed-os/targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F446xE/TARGET_NUCLEO_F446RE/PinNames.h
+~~~~
+
+staan de definities van algemene namen voor de SPI poort.
+Voor de `F446RE` zijn dit de SPI definities:
+
+~~~~
+typedef enum {
+    SPI_MOSI    = PA_7,
+    SPI_MISO    = PA_6,
+    SPI_SCK     = PA_5,
+    SPI_CS      = PB_6,
+} PinName;
+~~~~
+
+Alleen de SPI definities worden hierboven opgesomd.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 /* 
-   Midiplayer 7/ 7/2019
+   Midiplayer 21/ 8/2019
  */
 
 #include "mbed.h"
 
+#include <stdio.h>
+#include <errno.h>
+
+#if COMPONENT_SD
+#include "SDBlockDevice.h"
+#endif 
+
+
+/*
 DigitalOut led1(LED1);
 
 
@@ -17,3 +26,241 @@ int main()
       wait_ms(500);
    }
 }
+ */
+
+
+
+// This will take the system's default block device
+//BlockDevice *bd = BlockDevice::get_default_instance();
+
+#if COMPONENT_SD
+SDBlockDevice *bd = new SDBlockDevice(
+        MBED_CONF_SD_SPI_MOSI,
+        MBED_CONF_SD_SPI_MISO,
+        MBED_CONF_SD_SPI_CLK,
+        MBED_CONF_SD_SPI_CS);
+#endif
+
+// Instead of the default block device, you can define your own block device.
+// For example: HeapBlockDevice with size of 2048 bytes, read size 1, write size 1 and erase size 512.
+// #include "HeapBlockDevice.h"
+// BlockDevice *bd = new HeapBlockDevice(2048, 1, 1, 512);
+
+
+// This example uses LittleFileSystem as the default file system
+//#include "LittleFileSystem.h"
+//LittleFileSystem fs("fs");
+
+// Uncomment the following two lines and comment the previous two to use FAT file system.
+#include "FATFileSystem.h"
+FATFileSystem fs("fs");
+
+
+// Set up the button to trigger an erase
+InterruptIn irq(BUTTON1);
+void erase()
+{
+   printf("Initializing the block device... ");
+   fflush(stdout);
+   int err = bd->init();
+   printf("%s\n", (err ? "Fail :(" : "OK"));
+   if (err)
+   {
+      error("error: %s (%d)\n", strerror(-err), err);
+   }
+
+   printf("Erasing the block device... ");
+   fflush(stdout);
+   err = bd->erase(0, bd->size());
+   printf("%s\n", (err ? "Fail :(" : "OK"));
+   if (err)
+   {
+      error("error: %s (%d)\n", strerror(-err), err);
+   }
+
+   printf("Deinitializing the block device... ");
+   fflush(stdout);
+   err = bd->deinit();
+   printf("%s\n", (err ? "Fail :(" : "OK"));
+   if (err)
+   {
+      error("error: %s (%d)\n", strerror(-err), err);
+   }
+}
+
+
+// Entry point for the example
+int main()
+{
+   printf("--- Mbed OS filesystem example ---\n");
+
+   // Setup the erase event on button press, use the event queue
+   // to avoid running in interrupt context
+   irq.fall(mbed_event_queue()->event(erase));
+
+   // Try to mount the filesystem
+   printf("Mounting the filesystem... ");
+   fflush(stdout);
+   int err = fs.mount(bd);
+   printf("%s\n", (err ? "Fail :(" : "OK"));
+   if (err)
+   {
+      // Reformat if we can't mount the filesystem
+      // this should only happen on the first boot
+      printf("No filesystem found, formatting... ");
+      fflush(stdout);
+      err = fs.reformat(bd);
+      printf("%s\n", (err ? "Fail :(" : "OK"));
+      if (err)
+      {
+         error("error: %s (%d)\n", strerror(-err), err);
+      }
+   }
+
+   // Open the numbers file
+   printf("Opening \"/fs/numbers.txt\"... ");
+   fflush(stdout);
+   FILE *f = fopen("/fs/numbers.txt", "r+");
+   printf("%s\n", (!f ? "Fail :(" : "OK"));
+   if (!f)
+   {
+      // Create the numbers file if it doesn't exist
+      printf("No file found, creating a new file... ");
+      fflush(stdout);
+      f = fopen("/fs/numbers.txt", "w+");
+      printf("%s\n", (!f ? "Fail :(" : "OK"));
+      if (!f)
+      {
+         error("error: %s (%d)\n", strerror(errno), -errno);
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+         printf("\rWriting numbers (%d/%d)... ", i, 10);
+         fflush(stdout);
+         err = fprintf(f, "    %d\n", i);
+         if (err < 0)
+         {
+            printf("Fail :(\n");
+            error("error: %s (%d)\n", strerror(errno), -errno);
+         }
+      }
+      printf("\rWriting numbers (%d/%d)... OK\n", 10, 10);
+
+      printf("Seeking file... ");
+      fflush(stdout);
+      err = fseek(f, 0, SEEK_SET);
+      printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+      if (err < 0)
+      {
+         error("error: %s (%d)\n", strerror(errno), -errno);
+      }
+   }
+
+   // Go through and increment the numbers
+   for (int i = 0; i < 10; i++)
+   {
+      printf("\rIncrementing numbers (%d/%d)... ", i, 10);
+      fflush(stdout);
+
+      // Get current stream position
+      long pos = ftell(f);
+
+      // Parse out the number and increment
+      int32_t number;
+      fscanf(f, "%d", &number);
+      number += 1;
+
+      // Seek to beginning of number
+      fseek(f, pos, SEEK_SET);
+    
+      // Store number
+      fprintf(f, "    %d\n", number);
+
+      // Flush between write and read on same file
+      fflush(f);
+   }
+   printf("\rIncrementing numbers (%d/%d)... OK\n", 10, 10);
+
+   // Close the file which also flushes any cached writes
+   printf("Closing \"/fs/numbers.txt\"... ");
+   fflush(stdout);
+   err = fclose(f);
+   printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+   if (err < 0)
+   {
+      error("error: %s (%d)\n", strerror(errno), -errno);
+   }
+    
+   // Display the root directory
+   printf("Opening the root directory... ");
+   fflush(stdout);
+   DIR *d = opendir("/fs/");
+   printf("%s\n", (!d ? "Fail :(" : "OK"));
+   if (!d)
+   {
+      error("error: %s (%d)\n", strerror(errno), -errno);
+   }
+
+   printf("root directory:\n");
+   while (true)
+   {
+      struct dirent *e = readdir(d);
+      if (!e)
+      {
+         break;
+      }
+
+      printf("    %s\n", e->d_name);
+   }
+
+   printf("Closing the root directory... ");
+   fflush(stdout);
+   err = closedir(d);
+   printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+   if (err < 0)
+   {
+      error("error: %s (%d)\n", strerror(errno), -errno);
+   }
+
+   // Display the numbers file
+   printf("Opening \"/fs/numbers.txt\"... ");
+   fflush(stdout);
+   f = fopen("/fs/numbers.txt", "r");
+   printf("%s\n", (!f ? "Fail :(" : "OK"));
+   if (!f)
+   {
+      error("error: %s (%d)\n", strerror(errno), -errno);
+   }
+
+   printf("numbers:\n");
+   while (!feof(f))
+   {
+      int c = fgetc(f);
+      printf("%c", c);
+   }
+
+   printf("\rClosing \"/fs/numbers.txt\"... ");
+   fflush(stdout);
+   err = fclose(f);
+   printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+   if (err < 0)
+   {
+      error("error: %s (%d)\n", strerror(errno), -errno);
+   }
+
+   // Tidy up
+   printf("Unmounting... ");
+   fflush(stdout);
+   err = fs.unmount();
+   printf("%s\n", (err < 0 ? "Fail :(" : "OK"));
+   if (err < 0)
+   {
+      error("error: %s (%d)\n", strerror(-err), err);
+   }
+        
+   printf("Mbed OS filesystem example done!\n");
+}
+
+
+
